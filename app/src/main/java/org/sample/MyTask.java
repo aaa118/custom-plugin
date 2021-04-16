ebdde6ef0d0de0a9b28926c039075557c3865563
aaa118 <aaavs118@gmail.com>
2021-04-15 20:56:27 -0400

    first commit

---
+++ app/src/main/java/org/sample/MyTask.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public class MyTask extends DefaultTask {
+
+    @TaskAction
+    public void myTask() {
+        System.out.println("Hello World");
+    }
+}
