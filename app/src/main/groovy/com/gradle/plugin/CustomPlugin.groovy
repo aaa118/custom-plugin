7bc7bb62c37fe355da3879b528b380be4d7f417a
aaa118 <aaavs118@gmail.com>
2021-04-16 15:50:09 -0400

    Updating only version code

--- app/src/main/groovy/com/gradle/plugin/CustomPlugin.groovy
+++ app/src/main/groovy/com/gradle/plugin/CustomPlugin.groovy
@@ -33,9 +33,15 @@ class CustomPlugin implements Plugin<Project> {
             final def currentVersionCode = versionsProperties.getProperty("${versionPrefix}_code", "100000")
             final def currentBuildNumber = versionsProperties.getProperty("${versionPrefix}_build", 1.toString())
             int buildNumber = currentBuildNumber.toInteger()
-            int major = currentVersionCode.toInteger() / 100000
-            major += 1
-            int version = major * 100000
+
+            int version
+            if (gitBranch() == 'master') {
+                int major = currentVersionCode.toInteger() / 100000
+                major += 1
+                version = major * 100000
+            } else {
+                version = currentVersionCode.toInteger()
+            }
 
             buildNumber += 1
             String newBuildNumberStr = String.format("%03d", buildNumber)
@@ -58,6 +64,15 @@ class CustomPlugin implements Plugin<Project> {
         properties.load(file.newDataInputStream())
 
         return properties
+    }
+
+    def gitBranch() {
+        def branch = ""
+        def proc = "git rev-parse --abbrev-ref HEAD".execute()
+        proc.in.eachLine { line -> branch = line }
+        proc.err.eachLine { line -> println line }
+        proc.waitFor()
+        branch
     }
 
 
